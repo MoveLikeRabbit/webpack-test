73e382314ecf6c3c78fbefbe9010aeda169e3a5f
司颖 <31060269+MoveLikeRabbit@users.noreply.github.com>
2021-07-19 08:36:27 +0800

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+// webpack 基于 node common.js 语法 （这里用 module.exports)
+module.exports = {
+  // 默认production 自动压缩文件
+  mode: 'production',
+  // 入口文件
+  entry: './src/index.js',
+  output: {
+    // 自定义打包文件名
+    filename: 'index.js',
+    // 写绝对路径
+    path: path.resolve(__dirname + '/dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          { loader: 'file-loader' },
+        ]
+      }
+    ]
+  }
+
+};
